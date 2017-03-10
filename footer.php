ac54f839ea079a1d8bb61a099768544492f97c93
Aimee Matthew-John <aimeebethmj@gmail.com>
2017-03-10 20:40:32 +0000

    Made a footer!

--- footer.php
+++ footer.php
@@ -1,4 +1,19 @@
-<!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<footer class="container">
+		<div class="row">
+	      	<div class="twelve columns h-centred">
+	      		<h3 class="">
+		            <span>Hello lovely!</span> 
+		            <span>I’m Aimee. I’m on a journey to</span>
+		            <span>share my gifts,</span> 
+		            <span>be <i>braver</i>, more <i>creative</i> and</span> 
+		            <span>enjoy every moment...</span><!-- <br>  -->
+		            <span>Oh hey, you too? :)</span>
+	          	</h3>
+	      		<!-- <h2>Oh hey, you too?    :)</h2>
+	      		 <img src="<?php theActiveThemeDirectory(); ?>images/AimeeTodayOhHey.svg">  -->
+	      	</div>
+	    </div>
+	</footer>
+
 </body>
 </html>

76c6e35004f0ae20e7ed3a5d853fa1db0b9fa01c
Aimee Matthew-John <aimeebethmj@gmail.com>
2017-03-18 19:27:40 +0000

    Home link show on scroll, hide at top

--- footer.php
+++ footer.php
@@ -2,12 +2,14 @@
 		<div class="row">
 	      	<div class="twelve columns h-centred">
 	      		<h3 class="">
+	      			<a href="<?php echo site_url(); ?>/about">
 		            <span>Hello lovely!</span> 
 		            <span>I’m Aimee. I’m on a journey to</span>
 		            <span>share my gifts,</span> 
 		            <span>be <i>braver</i>, more <i>creative</i> and</span> 
 		            <span>enjoy every moment...</span><!-- <br>  -->
 		            <span>Oh hey, you too? :)</span>
+		            </a>
 	          	</h3>
 	      		<!-- <h2>Oh hey, you too?    :)</h2>
 	      		 <img src="<?php theActiveThemeDirectory(); ?>images/AimeeTodayOhHey.svg">  -->
@@ -15,5 +17,30 @@
 	    </div>
 	</footer>
 
+	<script src="<?php theActiveThemeDirectory(); ?>js/jquery-3.2.0.slim.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() 
+		{
+			if (jQuery) 
+			{  
+	          	// jQuery is loaded  
+	          	console.log("jQuery yas")
+	        
+				$(window).scroll( function()
+				{
+					console.log('scrolling')
+					var scrollTop = $(this).scrollTop()
+					var opacity = '1'
+					if (scrollTop < 20) opacity = '0' 
+					$('#home-link').css({'opacity': opacity})
+				})
+	        } 
+	        else 
+	        {
+	          	// jQuery is not loaded
+	          	console.log("jQuery nope")
+	        }
+		})
+	</script>
 </body>
 </html>
